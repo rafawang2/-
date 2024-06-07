47bcd36c0898dc0d0c8aed78cb46be4b709612bf
rafawang2 <rafawcy@gmail.com>
2024-06-08 01:39:13 +0800

    update comment

--- AudioAnalysis.py
+++ AudioAnalysis.py
@@ -33,7 +33,7 @@ def note_name(note):  # 把音的值轉為音名
 def note_to_fftbin(note):
     return note_number_to_freq(note) / FREQ_STEP
 
-# Get min/max index within FFT of notes we care about.
+# 限制最高及最低音之音頻，降低不必要之計算
 NOTE_MIN_BIN = max(0, int(np.floor(note_to_fftbin(NOTE_MIN - 1))))
 NOTE_MAX_BIN = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX + 1))))
 
@@ -41,7 +41,7 @@ NOTE_MAX_BIN = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX + 1))))
 buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)   #buf(32768,0)
 num_frames = 0
 
-# Initialize audio
+# 設置麥克風輸入
 stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
                                 channels=1,
                                 rate=FSAMP,
@@ -50,52 +50,49 @@ stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
 
 stream.start_stream()
 
-# Set up the plot
+# 畫圖
 fig, ax = plt.subplots()
 x = np.arange(0, 2 * FRAME_SIZE, 2)
 line, = ax.plot(x, np.random.rand(FRAME_SIZE), '-', lw=2)
-ax.set_ylim(-500, 500)
+ax.set_ylim(-500, 500)  #振幅
 ax.set_xlim(0, FRAME_SIZE)
 plt.xlabel('Samples')
 plt.ylabel('Amplitude')
 
-# Function to update the plot
+# 更新波形圖
 def update_plot(frame):
     global buf, num_frames
 
-    # Shift the buffer down and new data in
+    # 平移資料，舊資料往後推，新資料放前面
     buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
     buf[-FRAME_SIZE:] = np.frombuffer(stream.read(FRAME_SIZE), np.int16)
 
-    # Run the FFT on the windowed buffer
+    # 計算FFT
     fft = np.fft.rfft(buf)
 
-    # Get frequency of maximum response in range
+    # 取影響力最大之頻率
     max_response_index = np.abs(fft[NOTE_MIN_BIN:NOTE_MAX_BIN]).argmax()
     freq = (max_response_index + NOTE_MIN_BIN) * FREQ_STEP
 
-    # Get note number and nearest note
+    # 預估音高
     note_number = freq_to_note_number(freq)
     nearest_note = int(round(note_number))
 
-    # Console output once we have a full buffer
+    # 處理下一幀
     num_frames += 1
 
     if num_frames >= FRAMES_PER_FFT:
-        if np.max(np.abs(buf)) > THRESHOLD:
+        if np.max(np.abs(buf)) > THRESHOLD: #過濾雜訊，以免印出過多不必要的資訊
             print('Frequency: {:7.2f} Hz     Note: {:>3s} {:+.2f}'.format(freq, note_name(nearest_note), note_number - nearest_note))
 
-    # Update the plot with the latest frame
+    # 更新新波形
     line.set_ydata(buf[-FRAME_SIZE:])
     return line,
 
-# Create the animation
 ani = FuncAnimation(fig, update_plot, blit=True, interval=50)
 
-# Show the plot
 plt.show()
 
-# Close the stream gracefully on exit
 stream.stop_stream()
 stream.close()
 pyaudio.PyAudio().terminate()
